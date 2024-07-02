dbc31362934c8d4b754cfebb08b2748804ceed8c
Georgina Mudditt <g.k.mudditt@gmail.com>
2024-07-02 14:51:52 +0200

    add screenshots for ex 5, 6, 7, 8

--- Exercise9/script.js
+++ Exercise9/script.js
@@ -1,3 +1,11 @@
 // votre code ici
-
+function checkAge(age) {
+    if (age < 18) {
+        return "You are under 18";
+    } else if (age >= 18 && age <= 65) {
+        return "You are an adult";
+    } else {
+        return "You are a senior";
+    }
+}
 export default checkAge;
